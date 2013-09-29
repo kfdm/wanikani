7e98e9046cef29170522b0e9d0af14d4d55bc01c
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-09-28 22:35:27 -0700

    Initial project stub

---
+++ setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+    kwargs = {
+        'entry_points':{
+            'console_scripts': [
+                'wk = wanikani.cli:main'
+            ]
+        }
+    }
+except ImportError:
+    from distutils.core import setup
+    kwargs = {
+        'scripts':['scripts/wk']
+    }
+
+__version__ = '0.1'
+
+setup(
+    name='wanikani',
+    description='Python Library for WaniKani',
+    long_description=open('README.rst').read(),
+    author='Paul Traylor',
+    url='http://github.com/kfdm/wanikani/',
+    version=__version__,
+    packages=['wanikani'],
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+    ],
+    **kwargs
+)
