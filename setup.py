9518176e9ecb101e3bb079b2e8cce657556388c9
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-01-01 16:40:28 +0900

    Update with optional django app

--- setup.py
+++ setup.py
@@ -1,23 +1,10 @@
-try:
-    from setuptools import setup
-    kwargs = {
-        'entry_points': {
-            'console_scripts': [
-                'wk = wanikani.cli:main'
-            ]
-        }
-    }
-except ImportError:
-    from distutils.core import setup
-    kwargs = {
-        'scripts': ['scripts/wk']
-    }
+from setuptools import setup
 
 __version__ = '0.1'
 
 setup(
     name='wanikani',
-    description='Python Library for WaniKani',
+    description='WaniKani Tools for Python',
     long_description=open('README.md').read(),
     author='Paul Traylor',
     url='http://github.com/kfdm/wanikani/',
@@ -33,5 +20,20 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
     ],
-    **kwargs
+    entry_points={
+        'console_scripts': [
+            'wk = wanikani.cli:main',
+            'wanikani = wanikani.django.manage:main [django]',
+        ]
+    },
+    extras_require={
+        'django': [
+            'dj_database_url',
+            'Django >= 1.9, < 1.10',
+            'django-cache-url',
+            'envdir',
+            'python-social-auth',
+            'raven',
+        ],
+    }
 )
